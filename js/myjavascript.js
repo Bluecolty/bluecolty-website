0fa6aa973bc0c9063817d1edbe90a16728e21a2d
bluecolty <roy.strofferson@gmail.com>
2023-04-25 23:24:25 -0400

    heccin

--- js/myjavascript.js
+++ js/myjavascript.js
@@ -1,4 +1,4 @@
 document.addEventListener('scroll',function(){
     var scroll = window.pageYOffset;
-    document.querySelector('.stills-banner').style.filter = 'blur('+ scroll/50 +'px)';
+    document.querySelector('.blur-banner').style.filter = 'blur('+ scroll/50 +'px)';
 })
